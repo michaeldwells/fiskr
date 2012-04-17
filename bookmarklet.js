6dd65d72a58f4a7aa5749374bb7f268f0afc52c2
Michael Wells <michael.d.wells@gmail.com>
2012-04-17 18:32:52 +0100

    created bookmarklet.js

---
+++ bookmarklet.js
@@ -0,0 +1,11 @@
+(function( originalWindow, undefined ) {
+
+var window = originalWindow;
+var document = window.document;
+
+//var p = document.createElement('p');
+//p.innerHTML = "Hello";
+//document.body.appendChild(p);
+document.body.innerHTML = "<p>Hello</p>";
+
+}(window));
