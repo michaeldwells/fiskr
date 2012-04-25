289a741f824e61c5e0e805afda4b2d3332c7e10a
Michael Wells <michael.d.wells@gmail.com>
2012-04-25 18:06:00 +0100

    Added comments. Testing limiting the effect to selected text.

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,28 +1,40 @@
 (function( originalWindow, undefined ) {
 
+
 var window = originalWindow;
 var document = window.document;
 
-//var p = document.createElement('p');
-//p.innerHTML = "Hello";
-//document.body.appendChild(p);
+var sel = window.getSelection();
 
-//document.body.innerHTML = "<p>Hello</p>";
-
+// Get the non-trivial text content from every leaf node
 var allNodes = document.querySelectorAll('*');
 var texts = [];
-for (var i in allNodes) {
-    if (allNodes[i].childElementCount == 0) {
-        texts.push(allNodes[i].textContent);
+for (var i in allNodes)
+{
+    if (allNodes[i].childElementCount == 0)
+    {
+        if ((allNodes[i].textContent != "") and
+            (allNodes[i].tagName.toLowerCase() != "script") and
+            (allNodes[i].tagName.toLowerCase() != "style"))
+        {
+            if (sel.isCollapsed or sel.containsNode(allNodes[i], true))
+            {
+                texts.push(allNodes[i].textContent);
+            }
+        }
     }
 }
 
+// Clear the body
 document.body.innerHTML = "";
 
-for (var i in texts) {
+// Populate the body with just the text from the original document
+for (var i in texts)
+{
     var p = document.createElement('p');
     p.innerHTML = texts[i];
     document.body.appendChild(p);
 }
+
 
 }(window));
