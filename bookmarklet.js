a2b7ff759fc61168d2d1874d9d8c0af6acfed798
Michael Wells <michael.d.wells@gmail.com>
2012-04-25 15:56:41 +0100

    Now does HTML to text maybe?

--- bookmarklet.js
+++ bookmarklet.js
@@ -6,6 +6,20 @@ var document = window.document;
 //var p = document.createElement('p');
 //p.innerHTML = "Hello";
 //document.body.appendChild(p);
-document.body.innerHTML = "<p>Hello</p>";
+
+//document.body.innerHTML = "<p>Hello</p>";
+
+var texts = []
+for n in document.querySelector('*') {
+    texts.append(n.textContent);
+}
+
+document.body.innerHTML = "";
+
+for t in texts {
+    var p = document.createElement('p');
+    p.innerHTML = t;
+    document.body.appendChild(p);
+}
 
 }(window));
